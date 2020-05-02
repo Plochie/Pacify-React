74601c1b8540732f3681151f51e93628dccc66c4
plochie <pareshlomate@gmail.com>
2020-05-03 00:27:26 +0530

    sync

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
 import { Button } from 'antd';
-import React from 'react';
+import React, { useState } from 'react';
 import SvgArea from 'src/components/SvgArea';
 import './App.scss';
 import TestArea from './components/TestArea/TestArea';
+import AddNewModuleModal from 'src/components/AddNewModuleModal/AddNewModuleModal';
 
 
 function App() {
+
+	const [addNewModuleModalVisible, setAddNewModuleModalVisible] = useState<boolean>(false);
+
 	return (
 		<div className='App'>
 			<SvgArea />
 			<TestArea />
-			<Button type="primary">Button</Button>
+			<Button type="primary" onClick={() => setAddNewModuleModalVisible(true)}>Add New Module</Button>
+			<AddNewModuleModal isVisible={addNewModuleModalVisible} setIsVisible={setAddNewModuleModalVisible} />
 		</div>
 	);
 }
